262a4ebfb3b64603580826d40028b40cc851875c
Agudrey <cristianagudelorey1@gmail.com>
2022-09-24 15:10:28 -0500

    first test of functioning

--- scripts.js
+++ scripts.js
@@ -4,6 +4,8 @@ const rock_Btn = document.getElementById('rock_button');
 const paper_Btn = document.getElementById('paper_button');
 const scissors_Btn = document.getElementById('scissors_button');
 
+const reset_btn = document.getElementById('reset_btn')
+
 
 // Characters
 
@@ -46,24 +48,26 @@ const machine_election = () =>{    //1,4,7 => rock    2,5,8 => paper    3,6,9 =>
 }
 
 
+
 const play = (user, machine, contH, contM) =>{
   let result;
 
+
   if(user === 'rock'){
     userImg.src = './imagenes/svg/rockstar_main.svg';
-
+  
     if(machine ==='rock'){
       robotImg.src = './imagenes/svg/rockstar_main.svg';
       result = 'Empate';
       return result;
-
+  
     }else if(machine === 'paper'){
       contM.push('*');
       score_machine.textContent = contM.length;
       robotImg.src = './imagenes/svg/toilet-paper_main.svg';
       result ='Perdiste :(';
       return result;
-
+  
     }else{
       contH.push('*');
       score_human.textContent = contH.length;
@@ -71,22 +75,22 @@ const play = (user, machine, contH, contM) =>{
       result = 'Ganaste :)';
       return result;
     }
-  
+    
   }else if(user === 'paper'){
     userImg.src = './imagenes/svg/toilet-paper_main.svg';
-
+  
     if(machine === 'rock'){
       contH.push('*');
       score_human.textContent = contH.length;
       robotImg.src = './imagenes/svg/rockstar_main.svg';
       result = 'Ganaste :)';
       return result;
-
+  
     }else if(machine === 'paper'){
       robotImg.src = './imagenes/svg/toilet-paper_main.svg';
       result ='Empate';
       return result;
-
+  
     }else{
       contM.push('*');
       score_machine.textContent = contM.length;
@@ -94,45 +98,117 @@ const play = (user, machine, contH, contM) =>{
       result = 'Perdiste :(';
       return result;
     }
-
+  
   }else{
     userImg.src = './imagenes/svg/scissor_main.svg';
-
+  
     if(machine === 'rock'){
       contM.push('*');
       score_machine.textContent = contM.length;
       robotImg.src = './imagenes/svg/rockstar_main.svg';
       result = 'Perdiste :(';
       return result;
-
+  
     }else if(machine === 'paper'){
       contH.push('*');
       score_human.textContent = contH.length;
       robotImg.src = './imagenes/svg/toilet-paper_main.svg';
       result ='Ganaste :)';
       return result;
-
+  
     }else{
       robotImg.src = './imagenes/svg/scissor_main.svg';
       result = 'Empate';
       return result;
     }
   }
+};
+
+
+const reset = (contH, contM) =>{
+  userImg.src = './imagenes/svg/user_main.svg';
+  robotImg.src = './imagenes/svg/robot_main.svg';
+
+  contH.splice(0);
+  contM.splice(0);
+
+  score_human.textContent = contH.length;
+  score_machine.textContent = contM.length;
+
+  console.log(contH.length);
+  console.log(contM.length);
+
 }
+
+// -----------------------------Start-------------------------------------- //
 
 
 let cont_h = [];
 let cont_m = [];
 
 
+
 rock_Btn.addEventListener('click', () =>{
-  console.log(play('rock', machine_election(), cont_h, cont_m));
+
+  if(cont_h.length < 10 && cont_m.length < 10){
+    console.log(play('rock', machine_election(), cont_h, cont_m));
+  }
+  
+  if(cont_h.length === 10 || cont_m.length === 10){
+    if(cont_h.length > cont_m.length){
+      text_result.textContent = 'HAS GANADO!';
+      text_result.style.color = 'var(--verde)';
+      return
+
+    }else if(cont_m.length > cont_h.length){
+      text_result.textContent = 'HAS PERDIDO';
+      text_result.style.color = 'var(--rojo)';
+      return
+    }
+  }
 });
 
 paper_Btn.addEventListener('click', () =>{
-  console.log(play('paper', machine_election(), cont_h, cont_m));
+
+  if(cont_h.length < 10 && cont_m.length < 10){
+    console.log(play('paper', machine_election(), cont_h, cont_m));
+  }
+  
+  if(cont_h.length === 10 || cont_m.length === 10){
+    if(cont_h.length > cont_m.length){
+      text_result.textContent = 'HAS GANADO!';
+      text_result.style.color = 'var(--verde)';
+      return
+
+    }else if(cont_m.length > cont_h.length){
+      text_result.textContent = 'HAS PERDIDO';
+      text_result.style.color = 'var(--rojo)';
+      return
+    }
+  }
 });
 
 scissors_Btn.addEventListener('click', () =>{
-  console.log(play('scissors', machine_election(), cont_h, cont_m));
+
+  if(cont_h.length < 10 && cont_m.length < 10){
+    console.log(play('scissors', machine_election(), cont_h, cont_m));
+  }
+  
+  if(cont_h.length === 10 || cont_m.length === 10){
+    if(cont_h.length > cont_m.length){
+      text_result.textContent = 'HAS GANADO!';
+      text_result.style.color = 'var(--verde)';
+      return
+
+    }else if(cont_m.length > cont_h.length){
+      text_result.textContent = 'HAS PERDIDO';
+      text_result.style.color = 'var(--rojo)';
+      return
+    }
+  }
+});
+
+
+reset_btn.addEventListener('click', () =>{
+  reset(cont_h, cont_m)
 });
